a8e3e43f2d713e1190f337acc03ffa4d8f44ee80
wizz9 <wizz990@gmail.com>
2023-02-10 10:26:22 +0100

    initial commit

---
+++ src/main/java/com/wz/asynctaskproc/AsyncTaskProcApplication.java
@@ -0,0 +1,13 @@
+package com.wz.asynctaskproc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsyncTaskProcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsyncTaskProcApplication.class, args);
+    }
+
+}
